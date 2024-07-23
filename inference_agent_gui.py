a4c0701ddb8ab6cfc7977cefecdaef43a4106445
Mart <phantichchai1998@gmail.com>
2024-07-23 23:37:10 +0700

    create capture program and convert to numpy array prepare for tensor vision input

---
+++ inference_agent_gui.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+import main
+
+def running():
+    window_title = clicked.get()
+    main.start_capturing(window_title, 100, 30)
+
+# Create the main window
+root = tk.Tk()
+root.title("Inference Agent")
+options = [
+    "Genshin Impact",
+    "Whthering Waves"
+]
+clicked = tk.StringVar()
+clicked.set("Genshin Impact")
+
+# Create and place the widgets
+tk.Label(root, text="Window Title:").grid(row=0, column=0, padx=10, pady=10)
+entry_window_title = tk.OptionMenu(root, clicked, *options)
+entry_window_title.grid(row=0, column=1, columnspan=1, padx=10, pady=10)
+
+tk.Label(root, text="Instruction:").grid(row=1, column=0, padx=10, pady=10)
+entry_instruction_text = tk.Entry(root)
+entry_instruction_text.grid(row=1, column=1, columnspan=1, padx=10, pady=10)
+
+start_btn = tk.Button(root, text="Start", command=running)
+start_btn.grid(row=1, column=2, columnspan=1, padx=10, pady=10)
+
+root.mainloop()
--- main.py
+++ main.py
@@ -103,6 +103,42 @@ def record_window(output_folder: str, window_title: str, label: Label, fps: int)
     out.release()
     label.config(text="Recording Stopped")
 
+def start_capturing(window_title: str, max_frames: int, fps: int):
+    # Run the capturing in a separate thread to avoid blocking the main Tkinter thread
+    capturing = threading.Thread(target=capture_from_window, args=(window_title, max_frames, fps))
+    capturing.start()
+
+
+def capture_from_window(window_title: str, max_frames: int, fps: int):
+    window_coords = get_window_coordinates(window_title)
+    if not window_coords:
+        raise Exception(f'Window with title "{window_title}" not found')
+
+    frames = []
+    frame_count = 0
+    interval = 1 / fps
+
+    with mss.mss() as sct:
+        while frame_count < max_frames:
+            start_time = time.time()
+            img = np.array(sct.grab(window_coords))
+            frame = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+            frames.append(frame)
+            frame_count += 1
+
+            cv2.imshow("Captured Frame", frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            
+            elapsed_time = time.time() - start_time
+            time.sleep(max(0, interval - elapsed_time))
+    
+    cv2.destroyAllWindows()
+
+    video_tensor = np.stack(frames, axis=0)
+    print(f"Video tensor shape: {video_tensor.shape}")
+    return video_tensor
+
 def save_log(output_logs):
     global key_log, mouse_log, frame_number
     with open(f"{output_logs}/key_log.json", 'w') as f:
