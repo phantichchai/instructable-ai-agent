bf4d2ab082ade0ff57dc6ae06906877deeb21592
phantichchai <phantichchai1998@gmail.com>
2025-04-17 21:22:04 +0700

    Update pre-trained weight of mineclip to use newer x_tranfromer library

---
+++ scripts/dev/print_tree.py
@@ -0,0 +1,18 @@
+import os
+
+
+def print_tree(root, prefix="", level=2):
+        if level < 0:
+            return
+        files = sorted(os.listdir(root))
+        for f in files:
+            if f in ['.git', 'dataset', "__pycache__"]:
+                continue
+            
+            path = os.path.join(root, f)
+            print(prefix + "├── " + f)
+            if os.path.isdir(path):
+                print_tree(path, prefix + "│   ", level - 1)
+
+if __name__ == "__main__":
+    print_tree(".", level=2)
