59f10a3c4beafcdecb4dbce84a78c8dd4abbfd73
Phantichchai <phantichchai1998@gmail.com>
2025-05-11 23:10:09 +0700

    Experiment 1: Use only behavior cloning with existing training data

--- scripts/training/genshin_policy_trainer.py
+++ scripts/training/genshin_policy_trainer.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     root_dir = './dataset'  # Path to your dataset
     image_size = (160, 256)  # Image size for the input
     batch_size = 8
-    num_epochs = 1
+    num_epochs = 2500
     learning_rate = 1e-3
     save_dir = './saved_models'  # Directory to save models
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
