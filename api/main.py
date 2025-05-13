87ce5ea217b6d91a3414f0e6fe3f078c49e94bfb
Phantichchai <phantichchai1998@gmail.com>
2025-05-13 23:48:57 +0700

    tag docker images as v0.1.0 and update docker able to use websockets

--- api/main.py
+++ api/main.py
@@ -14,7 +14,7 @@ mineclip = MineCLIP(**cfg).to(device)
 mineclip.load_state_dict(torch.load("saved_models/mineclip/attn_new.pth"))
 
 agent = PolicyFromMineCLIP(mineclip, 1024, 512, 22)
-agent.load_state_dict(torch.load('saved_models/PolicyFromMineCLIP_20250512_191107_epoch_500.pt'))
+agent.load_state_dict(torch.load('saved_models/PolicyFromMineCLIP_20250512_200159_epoch_750.pt'))
 agent.to(device)
 
 frame_buffer = deque(maxlen=16)
