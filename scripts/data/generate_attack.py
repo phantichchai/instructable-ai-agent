f854a07259f42e0db7ebb405b74efd7973e30637
phantichchai <phantichchai1998@gmail.com>
2025-05-16 21:36:52 +0700

    Update training data (video|metadata) for attack and jump action

--- scripts/data/generate_attack.py
+++ scripts/data/generate_attack.py
@@ -4,10 +4,15 @@ from tools.genshin.controller import GenshinImpactController
 import pyautogui
 import os
 import time
+import random
+
+attack_variantion = [
+    "Assault", "Strike", "Charge", "Assail", "Engage", "Pounce", "Lunge", "Attack"
+]
 
 controller = GenshinImpactController()
 dir = os.path.join("dataset", "attack")
-dataset_generator = GenerateDataset(controller=controller, dataset_dir=dir, fps=8, overwrite=True)
+dataset_generator = GenerateDataset(controller=controller, dataset_dir=dir, fps=8, overwrite=False)
 
 pyautogui.keyDown('alt')
 pyautogui.press('tab')
@@ -15,13 +20,15 @@ time.sleep(0.1)
 pyautogui.keyUp('alt')
 
 for switch in [(1, 2), (2, 3), (3, 4), (4, 1)]:
+    attack_label = random.choices(attack_variantion)
+
     dataset_generator.generate([
         (ActionMapping.NORMAL_ATTACK, 0.5),
         (ActionMapping.NORMAL_ATTACK, 0.5), 
         (ActionMapping.NORMAL_ATTACK, 0.5),
         (ActionMapping.NORMAL_ATTACK, 0.5), 
         (ActionMapping.NORMAL_ATTACK, 0.5),], 
-        "Attack", 1, 5, record_video=True)
+        attack_label, 1, 5, record_video=True)
 
     dataset_generator.generate(
         ActionMapping[f'SWITCH_CHARACTER_{switch[1]}'],
